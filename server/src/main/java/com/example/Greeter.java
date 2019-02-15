e417a6c31b4f2c0a70a5259bb3f9e3031219d843
Ganesh Sinouvassan <ganeshsinouvassan@Ganeshs-MacBook-Pro.local>
2019-02-15 13:46:03 +0000

    Added retun vaule on to the file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-   * @pram test comment
-   * @string test comment
+   * @param test comment
+   * @return test comment
    */
     public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
